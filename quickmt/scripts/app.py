f97c8fbe1e1bf1b0aa4ddcc13c0741e9bb86444f
radinplaid <franey.mark@gmail.com>
2025-03-24 22:02:16 -0400

    Add compute device option to app

--- quickmt/scripts/app.py
+++ quickmt/scripts/app.py
@@ -2,8 +2,6 @@ from pathlib import Path
 
 from fasthtml.common import *
 from fire import Fire
-
-# MonsterUI shadows fasthtml components with the same name
 from monsterui.all import *
 
 from quickmt import Translator
@@ -14,25 +12,27 @@ t = Translator(str(Path(model_folder) / models[0]))
 
 # Get frankenui and tailwind headers via CDN using Theme.blue.headers()
 hdrs = Theme.blue.headers()
-
-# fast_app is shadowed by MonsterUI to make it default to no Pico, and add body classes
-# needed for frankenui theme styling
 app, rt = fast_app(hdrs=hdrs)
 
 
-def translation_form(input_text: str = "", beam_size: int = 5, selected_model: str = None):
+def translation_form(input_text: str = "", beam_size: int = 5, selected_model: str = None, selected_device: str = None):
     return Form(
         Group(
             LabelSelect(
                 *Options("Select a model", *models, selected_idx=1, disabled_idxs={0}),
                 label="Model",
                 name="selected_model",
-                id="selected_model",
-                value=selected_model
+                value=selected_model,
             ),
             LabelInput("Beam Size", name="beam_size", value=beam_size),
+            LabelSelect(
+                *Options("Compute Device", *("cpu", "cuda"), selected_idx=1, disabled_idxs={0}),
+                label="Device",
+                name="selected_device",
+                value=selected_device,
+            ),            
             LabelTextArea(
-                "Input Text", name="input_text", value=input_text, placeholder="", style="min-height: 400px;"
+                "Input Text", name="input_text", style="min-height: 400px;"
             ),
             Button("Translate", cls=(ButtonT.primary, "mt-2")),
         ),
@@ -44,16 +44,12 @@ def translation_form(input_text: str = "", beam_size: int = 5, selected_model: s
 
 @rt
 def index():
-    # Titled using a H1 title, sets the page title, and wraps contents in Main(Container(...)) using
-    # frankenui styles. Generally you will want to use Titled for all of your pages
     return Titled(
         "quickmt",
         Grid(
             Div(translation_form(), style="margin-top: 20px;"),
             Div(
-                Form(
-                    LabelTextArea("Output Text", id="output_text", placeholder="", style="min-height: 600px;"),
-                ),
+                LabelTextArea("Output Text", id="output_text", style="min-height: 600px;"),
                 cols_lg=2,
                 style="margin-top: 20px;",
             ),
@@ -63,11 +59,11 @@ def index():
 
 @rt
 @app.post("/translate")
-def translate(input_text: str, beam_size: int, selected_model: str):
+def translate(input_text: str, beam_size: int, selected_model: str, selected_device: str):
     global t
     if str(selected_model) != str(t.model_path):
         print(f"Loading model {selected_model}")
-        t = Translator(str(Path(model_folder) / selected_model))
+        t = Translator(str(Path(model_folder) / selected_model), device=selected_device)
 
     return "\n".join(t(input_text.splitlines(), beam_size=beam_size))
 
