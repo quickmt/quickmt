38edd24904c72b3e80dda5b2d3e13a560582ff04
radinplaid <franey.mark@gmail.com>
2025-04-19 20:51:32 -0400

    Add model download functionality to app, simplify github icon

--- quickmt/scripts/app.py
+++ quickmt/scripts/app.py
@@ -6,16 +6,15 @@ from fire import Fire
 from monsterui.all import *
 from starlette.background import BackgroundTask
 from quickmt import Translator
-from quickmt.hub import hf_download, hf_list
+from quickmt.hub import hf_list, hf_download
 
 
-GITHUB_SVG = """<svg width="40px" height="40px" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#fff"/></svg>"""
+hdrs = Theme.green.headers(mode="dark", highlightjs=True)
+
+app, rt = fast_app(hdrs=hdrs, key_fname="/tmp/quickmt.sesskey")
+app.title = "quickmt"
 
 t = None
-
-# Get frankenui and tailwind headers via CDN using Theme.blue.headers()
-hdrs = Theme.green.headers(mode="dark", highlightjs=True)
-app, rt = fast_app(hdrs=hdrs, title="Test")
 
 
 def translation_form(
@@ -73,22 +72,12 @@ def translation_form(
     )
 
 
-def error_message(body: str, title: str, message_type: str = "error"):
-    if message_type == "error":
-        return Alert(DivLAligned(UkIcon("triangle-alert"), B(f"ERROR: {title}"), P(body)), cls=AlertT.error)
-    if message_type == "warning":
-        return Alert(DivLAligned(UkIcon("circle-alert"), B(f"WARNING: {title}"), P(body)), cls=AlertT.warning)
-    if message_type == "info":
-        return Alert(DivLAligned(UkIcon("info"), B(f"INFO: {title}"), P(body)), cls=AlertT.warning)
-    raise Exception("Message type must be error, warning or info")
-
-
 def menu_bar():
     return NavBar(
         Ul(
             Li(
                 A(
-                    NotStr(GITHUB_SVG),
+                    UkIcon("github", height=40, width=40),
                     href="https://github.com/quickmt/quickmt",
                     target="_blank",
                 ),
@@ -109,34 +98,65 @@ def index():
 
 
 @rt
+@app.post("/download_model")
+def download_model(session, selected_model: str, model_folder: str):
+    hf_download(model_name="quickmt/" + selected_model, output_dir=Path(model_folder) / selected_model)
+    return Alert(
+        DivLAligned(UkIcon("thumbsup"), B(f"Download complete"), P(f"Finished downloading {selected_model}")),
+        cls=AlertT.success,
+    )
+
+
+def model_download_panel(selected_model: str, model_folder: str):
+    return Alert(
+        DivLAligned(UkIcon("triangle-alert"), B(f"Model not found"), P("Model must be downloaded first.")),
+        cls=AlertT.error,
+    ), Form(
+        LabelInput("", name="model_folder", value=model_folder, hidden=True),
+        LabelInput(
+            "",
+            hidden=True,
+            name="selected_model",
+            value=selected_model,
+        ),
+        Button("Download Model", cls=(ButtonT.primary, "mt-2")),
+        method="post",
+        hx_post="/download_model",
+        hx_target="#output_text",
+    )
+
+
+@rt
 @app.post("/translate")
 def translate(
-    input_text: str, beam_size: int, selected_model: str, selected_device: str, num_threads: int, model_folder: str
+    session,
+    input_text: str,
+    beam_size: int,
+    selected_model: str,
+    selected_device: str,
+    num_threads: int,
+    model_folder: str,
 ):
     global t
     model_path = Path(model_folder) / selected_model
 
     if not model_path.exists():
-        return error_message(
-            title="Model not found",
-            body="Model must be downloaded first.",
-        ), render_md(
-            f"""```bash
-# Code to download model:
-quickmt-model-download quickmt/{selected_model} --output-dir {model_path}
-```"""
-        )
+        return model_download_panel(selected_model, model_folder)
 
     if t is None or str(selected_model) != str(t.model_path):
-        print(f"Loading model {selected_model}")
-
         try:
             t = Translator(str(model_path), device=selected_device, inter_threads=int(num_threads))
         except:
             if selected_device in ("auto", "gpu"):
-                return error_message(
-                    title="Could not load model",
-                    body="Be sure the model is fully downloaded, the cuda toolkit is installed and you have enough available GPU RAM.",
+                return Alert(
+                    DivLAligned(
+                        UkIcon("triangle-alert"),
+                        B(f"Could not load model"),
+                        P(
+                            "Be sure the model is fully downloaded, the cuda toolkit is installed and you have enough available GPU RAM."
+                        ),
+                    ),
+                    cls=AlertT.error,
                 )
 
     return LabelTextArea(
